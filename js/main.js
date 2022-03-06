5f7ceb648bf7246b89140f92606a72b6a5c42a96
nishi18masa@gmail.com <masa18nishi@gmail.com>
2022-03-06 13:16:57 +0900

    second commit

--- js/main.js
+++ js/main.js
@@ -22,20 +22,60 @@
 // }
 
 // 関数で1~20までの値を繰り返し出力するプログラムを記述する
-let num = function (i) {
-  if (i >= 1 && i <= 20) {
-    document.write("正しく入力されました" + "<br>");
-  } else {
-    document.write("1から20の間の数値を入れて下さい" + "<br>");
-  }
-}
+// let num = function (i) {
+//   if (i >= 1 && i <= 20) {
+//     document.write("正しく入力されました" + "<br>");
+//   } else {
+//     document.write("1から20の間の数値を入れて下さい" + "<br>");
+//   }
+// }
 
-num(21);
-num(11);
-num(2);
+// num(21);
+// num(11);
+// num(2);
 
 // if (answer >= 1 && answer <= 20) {
 //   document.write("正しく入力されました");
 // } else {
 //   document.write("1から20までで入力して下さい");
 // }
+
+// もしも50から1を引き続けて25以下になったら処理を止めるコードを書く
+// for (let num = 50; num >= 25; num--) {
+//   document.write(num + "<br>");
+// }
+
+// 3で割り切れるならば、yesを返し、10で割り切れればnoを、30で割り切れればyes/noを返す
+// for (let num = 50; num >= 25; num--) {
+//   if (num % 30 == 0) {
+//     document.write("yes/no" + "<br>");
+//   } else if (num % 3 == 0) {
+//     document.write("yes" + "<br>");
+//   } else if (num % 10 == 0) {
+//     document.write("no" + "<br>");
+//   } else {
+//     document.write(num + "<br>");
+//   }
+// }
+
+// 関数で上記の問題（ 3で割り切れるならば、yesを返し、10で割り切れればnoを、30で割り切れればyes/noを返す）を記述する
+
+let number = function (num) {
+  for (let num = 50; num >= 25; num--){
+    if (num % 30 == 0) {
+      document.write("yes/no" + "<br>");
+    } else if (num % 3 == 0) {
+      document.write("yes" + "<br>");
+    } else if (num % 10 == 0) {
+      document.write("no" + "<br>");
+    } else {
+      document.write(num + "<br>");
+    }
+  }
+}
+
+number();
+
+// number(33);
+// number(42);
+// number(50);
